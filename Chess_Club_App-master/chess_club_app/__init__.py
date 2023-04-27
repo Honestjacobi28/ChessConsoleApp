e7ad2066bdb08f2e4bba591e0c252800f152268b
jean-fabrice <jeanfabriceouffoue@gmail.com>
2023-04-27 19:32:35 +0000

    ajout de mon projet sur github firstcommit

---
+++ Chess_Club_App-master/chess_club_app/__init__.py
@@ -0,0 +1,5 @@
+import chess_club_app.views.main_menu as screen
+
+# afficher le menu principal
+def main():
+    screen.MainMenu()
---
+++ Chess_Club_App-master/chess_club_app/__main__.py
@@ -0,0 +1,9 @@
+import chess_club_app
+
+# vérifie que le module est exécuter en tant que programme principale
+# si oui l'application est démarrée
+if __name__ == '__main__':
+    chess_club_app.main() 
+    
+
+
